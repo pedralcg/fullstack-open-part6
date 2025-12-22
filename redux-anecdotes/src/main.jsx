afe5a6b4fd1a13128e59580a28550d44814ebc02
Pedro Alcoba <pedralcg@gmail.com>
2025-12-22 23:22:05 +0100

    Part06a - Ejer 6.10 Anecdotas, paso 8

--- redux-anecdotes/src/main.jsx
+++ redux-anecdotes/src/main.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createStore, combineReducers } from "redux";
-import { Provider } from "react-redux"; // 1. Importar el Provider
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
 
 import App from "./App";
 import anecdoteReducer from "./reducers/anecdoteReducer";
-import filterReducer from "./reducers/filterReducer";
+import filterSlice from "./reducers/filterReducer";
 
-const reducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  filter: filterReducer,
+const store = configureStore({
+  reducer: {
+    anecdotes: anecdoteReducer,
+    filter: filterSlice,
+  },
 });
 
-const store = createStore(reducer);
-
-// 2. Envolver <App /> con <Provider store={store}>
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
--- redux-anecdotes/src/reducers/filterReducer.js
+++ redux-anecdotes/src/reducers/filterReducer.js
@@ -1,17 +1,17 @@
-const filterReducer = (state = "ALL", action) => {
-  switch (action.type) {
-    case "SET_FILTER":
+import { createSlice } from "@reduxjs/toolkit";
+
+const filterSlice = createSlice({
+  name: "filter",
+  initialState: "ALL",
+  reducers: {
+    // Aquí defines directamente la lógica del cambio de estado
+    filterChange(state, action) {
       return action.payload;
-    default:
-      return state;
-  }
-};
+    },
+  },
+});
 
-export const filterChange = (filter) => {
-  return {
-    type: "SET_FILTER",
-    payload: filter,
-  };
-};
-
-export default filterReducer;
+// RTK crea automáticamente el action creator con el mismo nombre
+export const { filterChange } = filterSlice.actions;
+// Y el reducer listo para exportar
+export default filterSlice.reducer;
