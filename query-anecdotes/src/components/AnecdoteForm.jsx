1f521602764d74c6f9d36bee866ab2f6c5c19bcf
Pedro Alcoba <pedralcg@gmail.com>
2025-12-23 22:45:17 +0100

    Part06d - Ejer 6.21, paso 2

--- query-anecdotes/src/components/AnecdoteForm.jsx
+++ query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,9 +1,26 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { createAnecdote } from '../requests'
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+
+  // Definimos la mutación
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    // Cuando la mutación tiene éxito, invalidamos la caché
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    },
+  })
+
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
     console.log('new anecdote')
+
+    // Ejecutamos la mutación
+    newAnecdoteMutation.mutate({ content, votes: 0 })
   }
 
   return (
