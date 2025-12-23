aa55b89e10380a1493f863e5f5e7d5e79df83939
Pedro Alcoba <pedralcg@gmail.com>
2025-12-23 22:31:04 +0100

    Part06d - Ejer 6.20, paso 1

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -1,18 +1,33 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { getAnecdotes } from './requests'
 
 const App = () => {
-  const handleVote = (anecdote) => {
-    console.log('vote')
+  // 1. Definimos la consulta al servidor
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    retry: 1,
+  })
+
+  // 2. Manejamos el estado de carga
+  if (result.isLoading) {
+    return <div>loading data...</div>
   }
 
-  const anecdotes = [
-    {
-      content: 'If it hurts, do it more often',
-      id: '47145',
-      votes: 0,
-    },
-  ]
+  // 3. Manejamos el error (Ejercicio 6.20: Mensaje de servidor no disponible)
+  if (result.isError) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+
+  // Si llegamos aquí, los datos existen en result.data
+  const anecdotes = result.data
+
+  const handleVote = (anecdote) => {
+    console.log('vote', anecdote)
+  }
 
   return (
     <div>
